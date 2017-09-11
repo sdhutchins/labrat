b67361332743fdef1349e6258fe68e49d9aa6b86
sdhutchins <sdhutchins@users.noreply.github.com>
2017-09-11 18:26:34 -0500

    Updated to backup class. Added logzero

--- labrat/filemanager/backup_docs.py
+++ labrat/filemanager/backup_docs.py
@@ -2,6 +2,7 @@
 
 from shutil import copytree, SameFileError
 from logzero import logger as log
+import logzero
 from datetime import datetime as d
 import os
 import contextlib
@@ -12,13 +13,15 @@ class Backup(object):
         """Set up the logger."""
         format1 = '%a %b %d %I:%M:%S %p %Y'  # Used to add as a date
         format2 = '%m-%d-%Y_%I-%M-%S-%p'  # Used to append to archives
-        self.f1 = format1, self.f2 = format2
-        self.sourcedir = sourcedir, self.backupdir = backupdir
+        self.f1 = format1
+        self.f2 = format2
+        self.sourcedir = sourcedir
+        self.backupdir = backupdir
 
     def backup(self):
 
-        log.logfile("logs\directory_backup_%s.log" %
-                    str(d.now().strftime(self.f2)))
+        logzero.logfile("directory_backup_%s.log" %
+                        str(d.now().strftime(self.f2)))
         sep = 50*'-'
         log.info("#%s" % sep)
         log.info("The script name is %s" % os.path.basename(__file__))
@@ -26,6 +29,6 @@ class Backup(object):
                  str(d.now().strftime(self.f1)))
 
         with contextlib.suppress(OSError, SameFileError):
-            log.error()
+            log.error(str(OSError) or str(SameFileError))
             copytree(self.sourcedir, self.backupdir)
             log.info('%s has been backed up.' % self.sourcedir)
--- labrat/tests/test_backup.py
+++ labrat/tests/test_backup.py
@@ -1,2 +1,10 @@
 from labrat.filemanager import Backup
 from unittest import TestCase
+
+class TestBackup(TestCase):
+    def test_shutil(self):
+        self.assertEqual =
+
+
+if __name__ == '__main__':
+    unittest.main()
