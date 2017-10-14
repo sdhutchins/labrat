b0095b21f9e5648c2ae9383e5ff7ef75ad5e2cf5
karillith <azengyu@gmail.com>
2017-10-14 08:46:43 -0700

    Added function calculate_molarity
    Added function that calculates the molarity of a solution given moles and liters or milliliters

--- labrat/math/functions.py
+++ labrat/math/functions.py
@@ -16,3 +16,11 @@ def dilute_stock(cI, vI, **values):
             return final_volume
         else:
             raise KeyError('%s is not a valid key.' % key)
+            
+# Calculate the molarity of a solution given moles and liters or milliliters
+def calculate_molarity(moles, volume, unit_of_volume):
+    if (unit_of_volume == 'ml' or unit_of_volume == 'mL'):
+        volume = volume * 1000
+    elif (unit_of_volume != 'l' and unit_of_volume != 'L'):
+        raise UserWarning('This unit of measurement is not supported.')
+    return moles/volume            
