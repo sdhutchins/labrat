def325b31bf2155bbe44d07f93f60452c692756e
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-10-14 10:36:17 +0530

    Updated functions.py
    Added function for calculating refractive index.

--- labrat/math/functions.py
+++ labrat/math/functions.py
@@ -1,5 +1,5 @@
 """Reusable math functions for the molecular scientists."""
-
+import math
 
 def dilute_stock(cI, vI, **values):
     """Dilute a stock concentration."""
@@ -16,3 +16,20 @@ def dilute_stock(cI, vI, **values):
             return final_volume
         else:
             raise KeyError('%s is not a valid key.' % key)
+
+def refractive_index_prism(A, D, angle_measurement)
+	"""Calculate the refractive index of prism using angle of prism and minimum angle of deviation."""
+	
+	if (angle_measurement == 'rad'):
+		refractive_index = (math.sin((A + D)/2)) / math.sin(A/2)
+		return refractive_index
+			
+	elif (angle_measurement == 'deg'):
+		A = math.radians(A)
+		D = math.radians(D)
+			
+		refractive_index = (math.sin((A + D)/2)) / math.sin(A/2)
+		return refractive_index
+
+	else:
+		raise ValueError('The angle measurement has to be done in deg or rad format.')
