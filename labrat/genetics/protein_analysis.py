713cdf9b0bb91c473fcc1a976c2e9e10cc476f65
sdhutchins <sdhutchins@outlook.com>
2018-06-17 00:03:01 -0500

    Integrated import_json function

--- labrat/genetics/protein_analysis.py
+++ labrat/genetics/protein_analysis.py
@@ -1,37 +1,26 @@
+"""Protein Analysis functions."""
 
 import re
 
+from labrat.utils import import_json
+
 fasta_file = input('Type your fasta file name: ')
 
-CODONS = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'
-    }
+__codons_dict = import_json("codons.json")
 
-def dna2aminoacid(fasta_file, codons):
+CODONS = __codons_dict['CODONS']
+
+
+def dna2aminoacid(fasta_file, codons=CODONS):
     AAsequence = ''
     with open(fasta_file) as fileObj:
         for line in fileObj:
             line.strip()
         cont = fileObj.read()
-        sequenceSearch = re.compile(r'')                                                #need to complete
+        sequenceSearch = re.compile(r'')  # need to complete
         content = sequenceSearch.search(cont)
         print(content.group())
-        for i in range(0,len(content.group()),3):
+        for i in range(0, len(content.group()), 3):
             seq = cont[i:i+3]
             AAsequence += codons[seq]
     return AAsequence
