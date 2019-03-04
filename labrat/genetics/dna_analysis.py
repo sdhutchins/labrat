b2138cb50e276fd126ac772f80240fe40b658b35
sdhutchins <sdhutchins@outlook.com>
2019-03-03 23:54:45 -0600

    Updated parameter in dna_analysis.py

--- labrat/genetics/dna_analysis.py
+++ labrat/genetics/dna_analysis.py
@@ -1,10 +1,10 @@
 """DNA Analysis functions."""
 
 
-def ATGC_content(DNA):
+def ATGC_content(dna_sequence):
     """Returns ATGC contents in the form of a dict."""
     ATGCDict = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
-    for nucleotide in DNA:
+    for nucleotide in dna_sequence:
         try:
             ATGCDict[nucleotide] += 1
         except KeyError:
@@ -12,11 +12,11 @@ def ATGC_content(DNA):
     return ATGCDict
 
 
-def complementary(DNA):
+def complementary_dna(dna_sequence):
     """Create a complementary DNA sequence."""
     cDNA = []
     complementary = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
-    for nucleotide in DNA:
+    for nucleotide in dna_sequence:
         try:
             cDNA.append(complementary[nucleotide])
         except KeyError:
