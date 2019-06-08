abe26894ca12c8ab01e93942a39fcd2b798c7e06
Shaurita <ms.s.hutchins@gmail.com>
2019-06-07 19:30:33 -0500

    Renamed variables to DATEFMT.

--- labrat/__init__.py
+++ labrat/__init__.py
@@ -1,4 +1,4 @@
 
 # Package Global Variables
-LOGFORMAT1 = '%a %b %d %I:%M:%S %p %Y'  # Used to add as a date
-LOGFORMAT2 = '%m-%d-%Y_%I-%M-%S-%p'  # Used to append to archives
+_DATEFMT1 = '%a %b %d %I:%M:%S %p %Y'  # Used to add as a date
+_DATEFMT2 = '%m-%d-%Y_%I-%M-%S-%p'  # Used to append to archives
