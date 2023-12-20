bdb8ededef70f27d19750b2bbe941987dc3f8da6
sdhutchins <sdhutchins@uab.edu>
2023-12-19 22:28:12 -0600

    Update python version.

--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ setup(
         'Operating System :: Unix',
         'Natural Language :: English',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     project_urls={
             'Documentation': '',
