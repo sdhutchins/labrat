e5caec571955ad2184f9d84e80ab5c83ffc41121
sdhutchins <sdhutchins@uab.edu>
2026-09-04 23:51:40 -0500

    Add PubTator literature queries.

--- labrat/query/__init__.py
+++ labrat/query/__init__.py
@@ -1,12 +1,15 @@
 """Query public gene, variant, and biomedical literature resources."""
 
 from labrat.query.gene import query_gene
+from labrat.query.literature import PubTatorClient, query_literature
 from labrat.query.models import QueryError, QueryResult
 from labrat.query.variant import query_variant
 
 __all__ = [
+    "PubTatorClient",
     "QueryError",
     "QueryResult",
     "query_gene",
+    "query_literature",
     "query_variant",
 ]
